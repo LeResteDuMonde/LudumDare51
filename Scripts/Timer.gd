c263dfb5f82dd80962f6a25e697b425ad9b19935
Jules Pesin <jules.pesin@gmail.com>
2022-10-01 12:36:48 +0200

    Audio Timer

--- Scripts/Timer.gd
+++ Scripts/Timer.gd
@@ -4,9 +4,13 @@ const DRAWING_TIME = 10
 
 var paused = true
 var timer
+var timer_seconds
+
 signal new_customer()
 
 onready var label := $Label
+
+onready var timer_sound = [$Audio1,$Audio2]
 
 func _ready():
 	reset_timer()
@@ -18,6 +22,8 @@ func _process(delta):
 
 func process_timer(delta):
 
+	if(timer_seconds < 0):
+		second_passed()
 	if(timer < 3):
 		label.add_color_override("font_color", Color.red)
 	if timer <= 0:
@@ -25,6 +31,7 @@ func process_timer(delta):
 
 	label.text = str(timer).substr(0,3)
 	timer -= delta
+	timer_seconds -= delta
 
 func end_timer():
 	reset_timer()
@@ -33,4 +40,9 @@ func end_timer():
 func reset_timer():
 	label.add_color_override("font_color", Color.black)
 	timer = DRAWING_TIME
+	timer_seconds = 1
+
+func second_passed():
+	timer_seconds = 1
+	timer_sound[0].play()
 
