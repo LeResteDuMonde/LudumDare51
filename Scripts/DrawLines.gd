cda017c626a299a6ebeba772d62cc5311e24162e
Jules Pesin <jules.pesin@gmail.com>
2022-10-01 12:20:53 +0200

    audio Drawing

--- Scripts/DrawLines.gd
+++ Scripts/DrawLines.gd
@@ -8,6 +8,8 @@ var _inCanva := false
 var current_lines = []
 var _current_line: Line2D
 
+onready var sound_effect := $AudioStreamPlayer2D
+
 func _input(event: InputEvent) -> void:
 	if _inCanva && event is InputEventMouseButton:
 		_pressed = event.pressed
@@ -24,16 +26,23 @@ func _input(event: InputEvent) -> void:
 			_lines.add_child(_current_line)
 			_current_line.set_global_position(Vector2.ZERO)
 
+			if !sound_effect.playing:
+				sound_effect.play(rand_range(0,sound_effect.stream.get_length()))
+		else:
+			sound_effect.stop()
+
 	if event is InputEventMouseMotion && _pressed:
 		_current_line.add_point(get_global_mouse_position())
 
 func clear_lines():
+	sound_effect.stop()
 	for l in current_lines:
 		_lines.remove_child(l)
 
 func _on_Area2D_mouse_exited():
 	_pressed = false
 	_inCanva = false
+	sound_effect.stop()
 
 func _on_Area2D_mouse_entered():
 	_inCanva = true
